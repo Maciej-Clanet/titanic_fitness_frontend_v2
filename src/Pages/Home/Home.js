1a8812381fba32b25d41b2479aff51081ba6aef4
Maciej-Clanet <maciejclanet@gmail.com>
2024-11-12 16:54:26 +0000

    feature grid complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,8 @@ import WaveHeading from "../../Components/WaveHeading/WaveHeading"
 import FeatureGrid from "../../Components/FeatureGrid/FeatureGrid"
 import FeatureItem from "../../Components/FeatureItem/FeatureItem"
 import Treadmill from "../../Assets/images/treadmill.png"
+import Clock from "../../Assets/images/clock.png"
+import Notepad from "../../Assets/images/notepad.png"
 
 export default function Home(){
     return(
@@ -16,8 +18,8 @@ export default function Home(){
             <WaveHeading text="LET THESE BENEFITS SINK IN" />
             <FeatureGrid>
                 <FeatureItem img={Treadmill} text="State of the art equipment"/>
-                <FeatureItem/>
-                <FeatureItem columns={2}/>
+                <FeatureItem img={Clock} text="24/7 access"/>
+                <FeatureItem columns={2} img={Notepad} text="Detailed Note Keeping"/>
             </FeatureGrid>
         </>
     )
