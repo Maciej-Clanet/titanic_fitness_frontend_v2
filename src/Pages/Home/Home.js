fbd5d5ed84d9f5a1fd7c148fa37745a08d0c6863
Maciej-Clanet <maciejclanet@gmail.com>
2024-11-13 10:27:05 +0000

    articles section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,7 @@ import FeatureItem from "../../Components/FeatureItem/FeatureItem"
 import Treadmill from "../../Assets/images/treadmill.png"
 import Clock from "../../Assets/images/clock.png"
 import Notepad from "../../Assets/images/notepad.png"
+import ArticleThumbnail from "../../Components/ArticleThumbnail/ArticleThumbnail"
 
 export default function Home(){
     return(
@@ -21,6 +22,37 @@ export default function Home(){
                 <FeatureItem img={Clock} text="24/7 access"/>
                 <FeatureItem columns={2} img={Notepad} text="Detailed Note Keeping"/>
             </FeatureGrid>
+            <div id='articles-section'>
+                <h1>Gym is just the tip of the iceberg</h1>
+                <div className="divider"/>
+                <h2>Hit the books</h2>
+                <div id="articles-grid">
+                    <ArticleThumbnail 
+                    title="Sleep" 
+                    description="And why it's over rated" 
+                    link="/" />
+                    <ArticleThumbnail 
+                    title="Nutrition" 
+                    description="Why Wendy's and Monster Energy are optimal nutrition" 
+                    link="/" />
+                    <ArticleThumbnail 
+                    title="Mewing" 
+                    description="Don't stop the streak" 
+                    link="/" />
+                    <ArticleThumbnail 
+                    title="Leg Day" 
+                    description="To Do or Not to Do, that is the Question" 
+                    link="/" />
+                    <ArticleThumbnail 
+                    title="Swimming" 
+                    description="Could be useful..." 
+                    link="/" />
+                    <ArticleThumbnail 
+                    title="Rest times" 
+                    description="Why you should bring a sleeping bag to the gym" 
+                    link="/" />
+                </div>
+            </div>
         </>
     )
 }
