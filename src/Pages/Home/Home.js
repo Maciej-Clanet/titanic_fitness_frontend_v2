d327a277fd0d38453baea115fe8eb022bb041866
Maciej-Clanet <maciejclanet@gmail.com>
2024-11-22 09:44:27 +0000

    dasasf

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import "./Home.css"
 import HeroBanner from "../../Components/HeroBanner/HeroBanner"
 import HeroImage from "../../Assets/images/banner_main.png"
 import WaveHeader from "../../Components/WaveHeader/WaveHeader"
-
+import Treadmill from "../../Assets/images/treadmill.png"
 export default function Home(){
     return(
         <>
@@ -11,7 +11,22 @@ export default function Home(){
                 <h2>Crash through  your fitness goals</h2>
             </HeroBanner>
             <WaveHeader text="LET THESE BENEFITS SINK IN"/>
-            this is the home page
+            
+            <section className="feature-grid">
+                <div className="feature-container">
+                    <img src={Treadmill} />
+                    <span>State of the art equipment</span>
+                </div>
+                <div className="feature-container">
+                    <img src={Treadmill} />
+                    <span>State of the art equipment</span>
+                </div>
+                <div className="feature-container wide">
+                    <img src={Treadmill} />
+                    <span>State of the art equipment</span>
+                </div>
+            </section>
+
         </>
     )
 }
