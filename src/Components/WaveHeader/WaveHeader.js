e8514cab91b5af6cabf62d558aabcaaa14efa554
Maciej-Clanet <maciejclanet@gmail.com>
2024-11-22 09:09:59 +0000

    wave header

---
+++ src/Components/WaveHeader/WaveHeader.js
@@ -0,0 +1,5 @@
+import "./WaveHeader.css"
+
+export default function WaveHeader({text}){
+    return <div className="wave-header"><h2>{text}</h2></div>
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import "./Home.css"
 import HeroBanner from "../../Components/HeroBanner/HeroBanner"
 import HeroImage from "../../Assets/images/banner_main.png"
+import WaveHeader from "../../Components/WaveHeader/WaveHeader"
 
 export default function Home(){
     return(
@@ -9,6 +10,7 @@ export default function Home(){
                 <h1>UNSINKABLE GAINS</h1>
                 <h2>Crash through  your fitness goals</h2>
             </HeroBanner>
+            <WaveHeader text="LET THESE BENEFITS SINK IN"/>
             this is the home page
         </>
     )
