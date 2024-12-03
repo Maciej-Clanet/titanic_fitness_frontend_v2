610ebdfe1b5e31da70d4ed534710741452380764
Maciej-Clanet <maciejclanet@gmail.com>
2024-12-03 12:03:05 +0000

    finished footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import "./Footer.css"
+
+export default function Footer(){
+
+
+    return(
+        <footer id="main-footer">
+            <div className="footer-links">
+                <h3>Links</h3>
+                <div className="footer-divider"/>
+                <a href="#">Conctact Us</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Help</a>
+            </div>
+        </footer>
+    )
+}
