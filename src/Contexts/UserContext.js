e92a95273cfd07a2fd34e92882f5176ca57641ef
Maciej-Clanet <maciejclanet@gmail.com>
2025-01-09 12:14:02 +0000

    articles section css

--- src/Contexts/UserContext.js
+++ src/Contexts/UserContext.js
@@ -1,27 +1,27 @@
-import { createContext, useState} from "react"; 
-import { useNavigate } from "react-router-dom"; 
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const UserContext = createContext(); 
+export const UserContext = createContext();
 
-export function UserProvider({children}){ 
+export function UserProvider({ children }) {
     const navigate = useNavigate()
     const [user, setUser] = useState(
-        JSON.parse( localStorage.getItem("user") ) || null
+        JSON.parse(localStorage.getItem("user")) || null
     );
-    function login(userData){
-        localStorage.setItem("user", JSON.stringify(userData) )
+    function login(userData) {
+        localStorage.setItem("user", JSON.stringify(userData))
         setUser(userData);
         navigate("/profile");
     }
-    function logout(){
+    function logout() {
         localStorage.removeItem("user")
         setUser(null)
         navigate("/join")
     }
 
-    return( 
-        <UserContext.Provider value={{user, login, logout}}> 
-            {children} 
-        </UserContext.Provider> 
-    ) 
+    return (
+        <UserContext.Provider value={{ user, login, logout }}>
+            {children}
+        </UserContext.Provider>
+    )
 } 
