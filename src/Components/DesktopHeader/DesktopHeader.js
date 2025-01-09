aacd8005ebcf73e5165d66c0c96389fe8b5eb5bd
Maciej-Clanet <maciejclanet@gmail.com>
2025-01-09 16:55:28 +0000

    fasfasfsa

--- src/Components/DesktopHeader/DesktopHeader.js
+++ src/Components/DesktopHeader/DesktopHeader.js
@@ -3,6 +3,7 @@ import Logo from "../Logo/Logo"
 
 import { useContext } from "react";
 import { UserContext } from "../../Contexts/UserContext";
+import { Link } from "react-router-dom";
 
 export default function DesktopHeader() {
 
@@ -11,16 +12,15 @@ export default function DesktopHeader() {
     return (
         <header id="desktop-header">
             <Logo />
-            <a href="/" className="nav-link">HOME</a>
-            <a href="/workouts" className="nav-link">WORKOUTS</a>
-
+            <Link to="/" className="nav-link">HOME</Link>
+            <Link to="/workouts" className="nav-link">WORKOUTS</Link>
             {
                 user ?
                     <>
                         <a className="nav-link" onClick={logout}>LOGOUT</a>
-                        <a href="/profile" className="action-button">My Account</a>
+                        <Link to="/profile" className="action-button">My Account</Link>
                     </>
-                    : <a href="/join" className="action-button">JOIN</a>
+                    : <Link to="/join" className="action-button">JOIN</Link>
             }
 
         </header>
